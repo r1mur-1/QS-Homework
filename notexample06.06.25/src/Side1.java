5790c41ef1c102f87bec65e94871f2a8147a1cc8
r1mur-1 <ggnowp@yandex.ru>
2025-06-17 20:02:14 +0300

    commit feat: Задачи

--- notexample06.06.25/src/Side1.java
+++ notexample06.06.25/src/Side1.java
@@ -4,8 +4,24 @@ public class Side1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int num = scan.nextInt();
+        boolean flag = false;
+        int place = num;
 
+        int[] height = new int[num];
 
+        for (int i = 0; i < height.length; i++) {
+            height[i] = scan.nextInt();
+        }
+        int petyaHeight = scan.nextInt();
 
+        for (int i = 0; i < height.length; i++) {
+            if (petyaHeight > height[i]) {
+                place = 1;
+                System.out.println(place + 1);
+                return;
+            }
+        }
+        System.out.println(place);
     }
 }
